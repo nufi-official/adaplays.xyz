cd291d694f38a625612ca92bfb0d0ec9b3356376
Richard Izip <richardizip@gmail.com>
2024-12-09 10:59:41 +0100

    feat: update to SDK v0.6.0

--- pages/games/rps/index.tsx
+++ pages/games/rps/index.tsx
@@ -57,18 +57,21 @@ const Home: NextPage = () => {
             }}>
               Testing: Sign Message
             </Button>
-            <Button {...commonButtonProps} onClick={() => {
-               nufiCoreSdk.getApi().showWidget('opened')
+            <Button {...commonButtonProps} onClick={async () => {
+                const {showWidget} = await nufiCoreSdk.getWidgetApi()
+                showWidget('opened')
               }}>
               Testing: Open widget
             </Button>
-            <Button {...commonButtonProps} onClick={() => {
-                  nufiCoreSdk.getApi().showWidget('closed')
+            <Button {...commonButtonProps} onClick={async () => {
+                const {showWidget} = await nufiCoreSdk.getWidgetApi()
+                showWidget('closed')
               }}>
               Testing: Close widget
             </Button>
-            <Button {...commonButtonProps} onClick={() => {
-                alert(`Visibility status ${nufiCoreSdk.getApi().getWidgetVisibilityStatus()}`)
+            <Button {...commonButtonProps} onClick={async () => {
+                const {getWidgetVisibilityStatus} = await nufiCoreSdk.getWidgetApi()
+                alert(`Visibility status ${getWidgetVisibilityStatus()}`)
               }}>
               Testing: Widget visibility status
             </Button>
