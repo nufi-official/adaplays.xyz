684d04199587e7d3e0af4d510a2aa177c644d63e
Richard Izip <richardizip@gmail.com>
2025-07-02 10:53:30 +0200

    feat: update @nufi/sso-button-react

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ import Navbar from '../components/navbar'
 import { SessionProvider } from "next-auth/react"
 import { Session } from 'next-auth'
 import { headingTheme } from 'theme/components/heading'
-import '@nufi/sso-button-react/dist/style.css'
+import '@nufi/sso-button-react/style.css'
 
 const theme = extendTheme({
   fonts: {
